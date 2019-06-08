6bb94687707ff92a85d400d56f488c1dbf8eada7
zxxml <zxxml@outlook.com>
2019-06-08 11:55:14 +0800

    Update tests.

--- tests/utils.py
+++ tests/utils.py
@@ -4,10 +4,7 @@ from collections import OrderedDict
 from functools import wraps
 from json import JSONDecoder
 
-
-def load_ordered(filename):
-    decoder = JSONDecoder(object_pairs_hook=OrderedDict)
-    return decoder.decode(open(filename).read())
+import coverage
 
 
 def ignore_error(func):
@@ -18,3 +15,19 @@ def ignore_error(func):
         except Exception as e:
             print(type(e), str(e.args))
     return _ignore_error
+
+
+def show_coverage(func):
+    cov = coverage.Coverage()
+    @wraps(func)
+    def _show_coverage(*args, **kwargs):
+        cov.start()
+        func(*args, **kwargs)
+        cov.stop()
+        cov.report()
+    return _show_coverage
+
+
+def load_ordered(filename):
+    decoder = JSONDecoder(object_pairs_hook=OrderedDict)
+    return decoder.decode(open(filename).read())
